e0aac07936ef7647556502830c9833f56d1dda5d
Paulo Andrade <andrade.oluap@gmail.com>
2024-03-17 01:13:23 -0300

    First flight data + processing

--- dataCollection/dataCollection.ino
+++ dataCollection/dataCollection.ino
@@ -13,12 +13,17 @@
 #define led 2
 
 // Global variables
-Adafruit_MPU6050 mpu;
-volatile byte writing = HIGH;  // Writing on file
-String formatedData;
-
+Adafruit_MPU6050 MPU;
+byte Writing = HIGH;  // Writing on file
+String FormatedData;
+sensors_event_t a, g, t;
 const char* fileName = "/teste_bateria.csv";
 
+// PWM reading
+volatile long StartTime = 0;
+volatile long CurrentTime = 0;
+volatile long Pulses = 0;
+int PulseWidth = 0;
 
 // SD Functions
 void writeFile(fs::FS &fs, const char * path, const char * message){
@@ -26,7 +31,7 @@ void writeFile(fs::FS &fs, const char * path, const char * message){
 
     File file = fs.open(path, FILE_WRITE);
     if(!file){
-        Serial.println("Failed to open file for writing");
+        Serial.println("Failed to open file for Writing");
         return;
     }
     if(file.println(message)){
@@ -71,14 +76,17 @@ void setup(void) {
   }
     
   // Initialize MPU6050
-  if (!mpu.begin()) {
+  if (!MPU.begin()) {
     Serial.println("Failed to find MPU6050 chip");
+    if (MPU.setTemperatureStandby(true)) {
+      Serial.println("Temperature sensor in standby mode");
+    }
   }
 
   // MPU range and bandwidth config
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  MPU.setAccelerometerRange(MPU6050_RANGE_4_G);
+  MPU.setGyroRange(MPU6050_RANGE_500_DEG);
+  MPU.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   // Interruption
   pinMode(RCPin, INPUT);
@@ -86,27 +94,41 @@ void setup(void) {
 
   // Datalogger's Header
   writeFile(SD, fileName, "Time,Acel. X,Acel. Y,Acel. Z,Rot. X,Rot. Y,Rot. Z");
+
+  // Interruption of RCPin for better performance
+  attachInterrupt(RCPin, PulseTimer, CHANGE);
 }
 
 
 void loop() {
   // Get new sensor events with the readings
-  byte switchValue = pulseIn(RCPin, HIGH);
+  if (Pulses < 2000) {
+    PulseWidth = Pulses;  
+  }
+  Serial.println(PulseWidth);
   
-  if (switchValue < 220) {
+  if (PulseWidth > 1100) {
     digitalWrite(led, HIGH);
-    sensors_event_t a, g, t;
-    mpu.getEvent(&a, &g, &t);
+    MPU.getEvent(&a, &g, &t);
   
-    formatedData = String(millis()) + "," + String(a.acceleration.x) + "," + String(a.acceleration.y) + "," + String(a.acceleration.z)
+    FormatedData = String(millis()) + "," + String(a.acceleration.x) + "," + String(a.acceleration.y) + "," + String(a.acceleration.z)
                    + "," + String(g.gyro.x) + "," + String(g.gyro.y) + "," + String(g.gyro.z);
                    
-    Serial.println(formatedData);
+    Serial.println(FormatedData);
     Serial.println();
     
-    appendFile(SD, fileName, formatedData.c_str());
+    appendFile(SD, fileName, FormatedData.c_str());
   } else {
     digitalWrite(led, LOW);
     Serial.println("Aguardando...");  
+  }
+}
+
+
+void PulseTimer() {
+  CurrentTime = micros();
+  if (CurrentTime > StartTime) {
+    Pulses = CurrentTime - StartTime;
+    StartTime = CurrentTime;
   }
 }
